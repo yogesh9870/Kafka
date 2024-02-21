3e2f8faa7f9b82eeb3e437a097cb54066c164dff
yogesh9870 <125638428+yogesh9870@users.noreply.github.com>
2024-02-21 21:36:42 +0530

    Add files via upload

---
+++ Kafka-producer-consumer_final/Kafka-producer-consumer/src/main/java/com/kafka/kafkaproducerconsumer/controller/EventController.java
@@ -0,0 +1,29 @@
+package com.kafka.kafkaproducerconsumer.controller;
+
+import com.kafka.kafkaproducerconsumer.model.student;
+import com.kafka.kafkaproducerconsumer.service.KafkaMessagePublisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/producer-app")
+
+public class EventController {
+
+    @Autowired
+    private KafkaMessagePublisher publisher;
+
+    @GetMapping("/publish/{name}/{age}")
+    public ResponseEntity<?> publishMessage(@PathVariable String name, @PathVariable int age){
+        try{
+            student student = new student(name, age);
+            publisher.sendMessageToTopic(student.toString());
+            return ResponseEntity.ok("message published successfully ..");
+        }catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
+    }
+}
