b427dea8a6f6439e527f395b8ecb7243ba165ab0
yogesh9870 <125638428+yogesh9870@users.noreply.github.com>
2024-02-23 21:58:24 +0530

    Add files via upload

---
+++ kafka-consumer/src/main/java/com/kafka/kafkaconsumer/consumer/KafkaMessageListener.java
@@ -0,0 +1,18 @@
+package com.kafka.kafkaconsumer.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class KafkaMessageListener {
+
+    Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
+
+    @KafkaListener(topics = "Kafka-test-sheshu1", groupId = "demo-group")
+    public void consumer(String message){
+        log.info("consumer consume the message {}", message);
+    }
+}
